f3a06272bc59b545a94db3d7aa6935b52cb9e39d
AddyHerrera <addy.herrerapaz@cbtis72.edu.mx>
2022-09-25 02:28:53 -0500

    Active la vista /tabla y cree la clase Tabla.
    Active la vista /tabla de homeController, agregue atributos en la clase producto, y  cree un html llamado tabla.

--- src/main/java/net/itinajero/ProyectofinalApplication.java
+++ src/main/java/net/itinajero/ProyectofinalApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProyectoTiendaApplication {
+public class ProyectofinalApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProyectoTiendaApplication.class, args);
+		SpringApplication.run(ProyectofinalApplication.class, args);
 	}
 
 }
--- src/test/java/net/itinajero/ProyectofinalApplicationTests.java
+++ src/test/java/net/itinajero/ProyectofinalApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProyectoTiendaApplicationTests {
+class ProyectofinalApplicationTests {
 
 	@Test
 	void contextLoads() {
